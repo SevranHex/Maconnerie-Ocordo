c52be652d7e3806aaad2d4bccda670d3e67076a4
Gautier Yrondy <gautieryrondy@gmail.com>
2021-12-21 14:01:41 +0100

    Ajout du fichier style, ajout du footer/header dans l'index

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php 
-    $fichier = 'source.xml';
-    $contenu = simplexml_load_file($fichier);
+    if (file_exists('source.xml')) {
+        $fichierXml = 'source.xml';
+        $contenu = simplexml_load_file($fichierXml);
+    }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title></title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <footer>
+        
+    </footer>
+</body>
+
+</html>
